5eba1393d2c59af6d86c4f52b7d3c70ddbc0483a
Toothbrush <Toothbrush@users.noreply.github.com>
2022-07-19 14:46:15 +0100

    Revert "just casually destroying everything potentially :3"
    This reverts commit 958fcc25152f65ce04aad5b28585ba6037480b7a.

--- Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
+++
@@ -1,9 +0,0 @@
-namespace Adobe.Substance
-{
-    public static class Version
-    {
-        public const string PluginVersion = "3.0.0-0-g535939d";
-        public const string EngineVersion = "8.2.0 0138644d";
-        public const int ImporterVersion = 300;
-    }
-}
